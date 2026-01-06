0ab4eb498725a0210d95279aada7a54734556eb8
Ian Bharrat <ian.bharrat@gmail.com>
2026-01-06 17:26:09 -0500

    added more complex variables and integrated them into main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 resource "local_file" "pet" {
     filename = var.filename
-    content = var.content
+    content = var.file-content["statement2"]
 }
 
-resource "random_pet" "my-pet" {
-    prefix = var.prefix 
+resource "random_pet" "my_pet" {
+    prefix = var.prefix[2]
     separator = var.separator
     length = var.length
+}
+
+resource "local_file" "the_thing" {
+    filename = var.filename_movie
+    content = jsonencode(var.the_thing)
+}
+
+resource "local_file" "mushu" {
+    filename = var.filename_mushu
+    content = jsonencode(var.mushu)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,19 @@
 variable "filename" {
-    default = "C:/Terraform/pets.txt"
+    default = "C:/Terraform/scampteddy.txt"
     type = string
-    description = "the path of the local file"
+    description = "the path of scampteddy.txt"
+}
+
+variable "filename_movie" {
+    default = "C:/Terraform/movie.txt"
+    type = string
+    description = "the path of movie.txt"
+}
+
+variable "filename_mushu" {
+    default = "C:/Terraform/mushu.txt"
+    type = string
+    description = "the path of mushu.txt"
 }
 
 variable "content" {
@@ -9,7 +21,9 @@ variable "content" {
 }
 
 variable "prefix" {
-    default = "Mrs"
+    default = ["Mr", "Mrs", "Sir"]
+    type = list(string)
+    description = "prefix for random name"
 }
 
 variable "separator" {
@@ -19,3 +33,36 @@ variable "separator" {
 variable "length" {
     default = 2
 }
+
+variable "file-content" {
+    type = map(string)
+    default = {
+        "statement1" = "We love pets!"
+        "statement2" = "We love Scamp and Teddy!"
+    }
+}
+
+variable "mushu" {
+    type = object({
+        name = string
+        color = string
+        age = number
+        food = list(string)
+        is_silly = bool
+    })
+
+    default = {
+        name = "mushu"
+        color = "orange"
+        age = 6
+        food = ["fish", "chicken", "turkey"]
+        is_silly = true
+    }
+}
+
+variable the_thing {
+    type = tuple([number, string, bool])
+    default = [1982, "Horror/Mystery", true]
+}
+
+
